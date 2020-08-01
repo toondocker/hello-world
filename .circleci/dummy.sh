1b652192f06369ca2b6182de394622a2a111ba5a
toondocker <46616363+toondocker@users.noreply.github.com>
2020-08-01 17:06:31 -0400

    try amking config more generic

---
+++ .circleci/dummy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv sh
+# shellcheck shell=sh
+# dummy script so that I can use 'standard' CircleCI config.yml
+# which requires there to be at least one script file due to
+# the use of ShellCheck
+exit 0
